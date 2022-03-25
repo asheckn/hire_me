d8370322f0088c90bdf74a12487a9db9600b5920
Nyasha Asheck Hove <nyasha.ahove@gmail.com>
2022-03-25 08:24:34 +0200

    initial boiler plate

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -3,47 +3,46 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import '../constants/urls.dart';
-import '../models/user.dart';
 
 class ApiService{
 
-  Future<dynamic> getUsers() async {
-    String url = "$baseUrl/getAllUsers";
-    print("getting users");
-    final response = await http.get(Uri.parse(url));
-    print(response.body);
-
-    if (response.statusCode == 200 || response.statusCode == 400) {
-      print(json.decode(response.body));
-      return json.decode(response.body);
-    } else {
-      debugPrint(response.body.toString());
-      debugPrint("Failed with code: ${response.statusCode}");
-      return [];
-    }
-  }
-
-  Future<dynamic> updateUser(User user) async {
-
-    var endpointUrl = "$baseUrl/updateUser";
-
-
-    var requestUrl = endpointUrl;
-    final response = await http.put(Uri.parse(requestUrl),
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-          "Access-Control-Allow-Headers": "*",
-        },
-        body: user.toRawJson(),
-        );
-    if (response.statusCode == 200 || response.statusCode == 201) {
-      print("updating user");
-      return true;
-    } else {
-      debugPrint(response.body.toString());
-      return false;
-    }
-  }
+  // Future<dynamic> getUsers() async {
+  //   String url = "$baseUrl/getAllUsers";
+  //   print("getting users");
+  //   final response = await http.get(Uri.parse(url));
+  //   print(response.body);
+  //
+  //   if (response.statusCode == 200 || response.statusCode == 400) {
+  //     print(json.decode(response.body));
+  //     return json.decode(response.body);
+  //   } else {
+  //     debugPrint(response.body.toString());
+  //     debugPrint("Failed with code: ${response.statusCode}");
+  //     return [];
+  //   }
+  // }
+  //
+  // Future<dynamic> updateUser(User user) async {
+  //
+  //   var endpointUrl = "$baseUrl/updateUser";
+  //
+  //
+  //   var requestUrl = endpointUrl;
+  //   final response = await http.put(Uri.parse(requestUrl),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Accept": "application/json",
+  //         "Access-Control-Allow-Headers": "*",
+  //       },
+  //       body: user.toRawJson(),
+  //       );
+  //   if (response.statusCode == 200 || response.statusCode == 201) {
+  //     print("updating user");
+  //     return true;
+  //   } else {
+  //     debugPrint(response.body.toString());
+  //     return false;
+  //   }
+  // }
 
 }
--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,18 +1,16 @@
 
+import 'package:hire_me/api/api_service.dart';
 
-import 'package:myprivacy/api/api_service.dart';
-
-import '../models/user.dart';
 ApiService apiService = new ApiService();
 
 
-Future<List<User>> getUsers()async {
-  var result = await apiService.getUsers() as List;
-  List<User> users = result.map((i) => User.fromJson(i)).toList();
-  return users;
-}
-
-Future<bool> updateUser(User user) async {
-  bool result = await apiService.updateUser(user);
-  return result;
-}
+// Future<List<User>> getUsers()async {
+//   var result = await apiService.getUsers() as List;
+//   List<User> users = result.map((i) => User.fromJson(i)).toList();
+//   return users;
+// }
+//
+// Future<bool> updateUser(User user) async {
+//   bool result = await apiService.updateUser(user);
+//   return result;
+// }
